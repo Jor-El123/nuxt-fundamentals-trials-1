fb73ecf771b5cb976f11462cae24bcf369857fc3
Jor-El123 <jorrelsimounbencio@gmail.com>
2021-03-12 14:13:27 +0800

    first commit

---
+++ store/posts.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+    all: [ 
+        {
+            id: 'balut',
+            title: 'What is a Balut',
+            content: 'a paper that shows proof the person has voted'
+        },
+            
+        {
+            id: 'WhereIsIt',
+            title: 'Where is the sign-in button',
+            content: 'Somewhere below the password field'
+        },
+
+        {
+            id: 'how',
+            title: 'How can I fix this problem',
+            content: 'Please help me with this. I\'m stuck'
+        }
+    ]
+})
